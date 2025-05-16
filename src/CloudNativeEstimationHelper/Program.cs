994c8bd37287a47fb7b4e05f34ac981a236b0728
Marco Antonio Silva <marconsilva@outlook.com>
2025-05-16 12:08:29 +0100

    changed project to webassembly added publish pipeline

--- src/CloudNativeEstimationHelper/Program.cs
+++ src/CloudNativeEstimationHelper/Program.cs
@@ -1,3 +1,11 @@
-var builder = DistributedApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using CloudNativeEstimationHelper;
 
-builder.Build().Run();
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
